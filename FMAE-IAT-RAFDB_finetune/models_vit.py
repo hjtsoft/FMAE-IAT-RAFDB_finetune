9e27724ed21a3489dda20bb78b47a0d3641f41ce
Huang Jintao <2955356630@qq.com>
2026-03-02 19:25:09 +0800

    baseline

---
+++ FMAE-IAT-RAFDB_finetune/models_vit.py
@@ -0,0 +1,113 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+# --------------------------------------------------------
+# References:
+# timm: https://github.com/rwightman/pytorch-image-models/tree/master/timm
+# DeiT: https://github.com/facebookresearch/deit
+# --------------------------------------------------------
+
+from functools import partial
+
+import torch
+import torch.nn as nn
+from torch.autograd import Function
+
+import timm.models.vision_transformer
+
+
+class GradReverse(Function):
+    @staticmethod
+    def forward(ctx, x, lambd, **kwargs: None):
+        ctx.lambd = lambd
+        return x.view_as(x)
+
+    def backward(ctx, grad_output):
+        return grad_output * -ctx.lambd, None
+
+
+class VisionTransformer(timm.models.vision_transformer.VisionTransformer):
+    """ Vision Transformer with support for global average pooling
+    """
+    def __init__(self, global_pool=False, grad_reverse=0, num_classes=0, num_subjects=0, **kwargs):
+        super(VisionTransformer, self).__init__(**kwargs)
+
+        self.global_pool = global_pool
+        if self.global_pool:
+            norm_layer = kwargs['norm_layer']
+            embed_dim = kwargs['embed_dim']
+            self.fc_norm = norm_layer(embed_dim)
+
+            del self.norm  # remove the original norm
+
+        # Classifier head
+        self.AU_head = nn.Linear(kwargs['embed_dim'], num_classes)
+        self.grad_reverse = grad_reverse  # default is 1.0
+        print(f"using ID adversarial: {self.grad_reverse}" )
+        print(f"num classes: {num_classes}, num subjects: {num_subjects}")
+        if not self.grad_reverse == 0:
+            self.ID_head = nn.Linear(kwargs['embed_dim'], num_subjects)
+            print(f"activate ID adver head")
+
+
+    def forward_features(self, x):
+        B = x.shape[0]
+        x = self.patch_embed(x)
+
+        cls_tokens = self.cls_token.expand(B, -1, -1)  # stole cls_tokens impl from Phil Wang, thanks
+        x = torch.cat((cls_tokens, x), dim=1)
+        x = x + self.pos_embed
+        x = self.pos_drop(x)
+
+        for blk in self.blocks:
+            x = blk(x)
+
+        if self.global_pool:
+            x = x[:, 1:, :].mean(dim=1)  # global pool without cls token
+            outcome = self.fc_norm(x)
+        else:
+            x = self.norm(x)
+            outcome = x[:, 0]
+
+        return outcome
+
+    def forward(self, x):
+        x = self.forward_features(x)
+        AU_pred = self.AU_head(x)
+
+        if not self.grad_reverse == 0:
+            x = GradReverse.apply(x, self.grad_reverse)
+            ID_pred = self.ID_head(x)
+            return AU_pred, ID_pred
+        else:
+            return AU_pred
+
+
+def vit_small_patch16(**kwargs):
+    model = VisionTransformer(
+        patch_size=16, embed_dim=384, depth=12, num_heads=6, mlp_ratio=4, qkv_bias=True,
+        norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
+    return model
+
+
+def vit_base_patch16(**kwargs):
+    model = VisionTransformer(
+        patch_size=16, embed_dim=768, depth=12, num_heads=12, mlp_ratio=4, qkv_bias=True,
+        norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
+    return model
+
+
+def vit_large_patch16(**kwargs):
+    model = VisionTransformer(
+        patch_size=16, embed_dim=1024, depth=24, num_heads=16, mlp_ratio=4, qkv_bias=True,
+        norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
+    return model
+
+
+def vit_huge_patch14(**kwargs):
+    model = VisionTransformer(
+        patch_size=14, embed_dim=1280, depth=32, num_heads=16, mlp_ratio=4, qkv_bias=True,
+        norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
+    return model
---
+++ FMAE-IAT-RAFDB_finetune/util/datasets.py
@@ -0,0 +1,255 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+# --------------------------------------------------------
+# References:
+# DeiT: https://github.com/facebookresearch/deit
+# --------------------------------------------------------
+
+import os
+import PIL
+from PIL import Image
+import json
+import zipfile
+import io
+import numpy as np
+import lmdb
+
+import torch
+from torchvision import datasets, transforms
+from torch.utils.data import Dataset
+
+from timm.data import create_transform
+from timm.data.constants import IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD
+
+
+def build_dataset(is_train, args):
+    transform = build_transform(is_train, args)
+
+    root = os.path.join(args.data_path, 'train' if is_train else 'val')
+    dataset = datasets.ImageFolder(root, transform=transform)
+
+    print(dataset)
+
+    return dataset
+
+
+def build_transform(is_train, args):
+    mean = IMAGENET_DEFAULT_MEAN
+    std = IMAGENET_DEFAULT_STD
+    # train transform
+    if is_train:
+        # this should always dispatch to transforms_imagenet_train
+        transform = create_transform(
+            input_size=args.input_size,
+            is_training=True,
+            color_jitter=args.color_jitter,
+            auto_augment=args.aa,
+            interpolation='bicubic',
+            re_prob=args.reprob,
+            re_mode=args.remode,
+            re_count=args.recount,
+            mean=mean,
+            std=std,
+        )
+        return transform
+
+    # eval transform
+    t = []
+    if args.input_size <= 224:
+        crop_pct = 224 / 256
+    else:
+        crop_pct = 1.0
+    size = int(args.input_size / crop_pct)
+    t.append(
+        transforms.Resize(size, interpolation=PIL.Image.BICUBIC),  # to maintain same ratio w.r.t. 224 images
+    )
+    t.append(transforms.CenterCrop(args.input_size))
+
+    t.append(transforms.ToTensor())
+    t.append(transforms.Normalize(mean, std))
+    return transforms.Compose(t)
+
+
+class CustomDataset(Dataset):
+    def __init__(self, lmdb_path=None, transform=None):
+        self.env = lmdb.open(lmdb_path, readonly=True, lock=False, readahead=False, meminit=False)
+        self.transform = transform
+
+        with self.env.begin(write=False) as txn:
+            self.keys = [key for key, _ in txn.cursor()]
+
+    def __len__(self):
+        return len(self.keys)
+
+    def __getitem__(self, index):
+        with self.env.begin(write=False) as txn:
+            key = self.keys[index]
+            img_bytes = txn.get(key)
+
+        img = Image.open(io.BytesIO(img_bytes)).convert('RGB')
+
+        if self.transform:
+            img = self.transform(img)
+
+        return img
+
+
+def BP4D_AU_dataset(json_path, is_train, args):
+    transform = build_AU_transform(is_train, args)
+    dataset = BP4D_dataset(args.root_path, json_path, transform=transform)
+    return dataset
+
+
+def BP4D_plus_AU_dataset(json_path, is_train, args):
+    transform = build_AU_transform(is_train, args)
+    dataset = BP4D_plus_dataset(args.root_path, json_path, transform=transform)
+    return dataset
+
+def build_RAFDB_dataset(folder_path, is_train, args):
+    transform = build_AU_transform(is_train, args)
+    dataset = datasets.ImageFolder(root=folder_path, transform=transform)
+    print(dataset)
+    return dataset
+
+
+def build_AU_transform(is_train, args):
+    mean = IMAGENET_DEFAULT_MEAN
+    std = IMAGENET_DEFAULT_STD
+
+    # train transform
+    if is_train:
+        transform = create_transform(
+            input_size=args.input_size,
+            is_training=True,
+            color_jitter=args.color_jitter,
+            auto_augment=args.aa,
+            interpolation='bicubic',
+            re_prob=args.reprob,
+            re_mode=args.remode,
+            re_count=args.recount,
+            mean=mean,
+            std=std,
+        )
+        return transform
+    # eval transform
+    else:
+        return transforms.Compose(
+            [transforms.Resize([args.input_size, args.input_size]),
+             transforms.ToTensor(),
+             transforms.Normalize(mean, std)]
+        )
+
+class BP4D_dataset(Dataset):
+    """
+    accept the json file to construct the dataset.
+    Each line of the json file contains the image path and the AU labels.
+    """
+    def __init__(self, root_path, json_file, transform=None):
+        self.data = self._load_data(json_file)
+        print(f"building dataset from: {json_file}")
+        self.root_path = root_path
+        print(f"dataset path: {self.root_path}")
+        self.transform = transform
+        self.AUs = [1, 2, 4, 6, 7, 10, 12, 14, 15, 17, 23, 24]
+        self.IDs = ['F001', 'F002', 'F003', 'F004', 'F005', 'F006', 'F007', 'F008', 'F009', 'F010',
+                    'F011', 'F012', 'F013', 'F014', 'F015', 'F016', 'F017', 'F018', 'F019', 'F020',
+                    'F021', 'F022', 'F023',
+                    'M001', 'M002', 'M003', 'M004', 'M005', 'M006', 'M007', 'M008', 'M009', 'M010',
+                    'M011', 'M012', 'M013', 'M014', 'M015', 'M016', 'M017', 'M018']
+        self.AU_label2idx = {label: idx for idx, label in enumerate(self.AUs)}
+        self.ID_label2idx = {label: idx for idx, label in enumerate(self.IDs)}
+
+    def _load_data(self, json_file):
+        dict_list = []
+        with open(json_file, 'r') as file:
+            for line in file:
+                loaded_dict = json.loads(line)
+                dict_list.append(loaded_dict)
+        return dict_list
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        image_path = self.root_path + self.data[idx]['img_path']
+        image = Image.open(image_path).convert('RGB')
+
+        # Convert label indices to binary representation
+        AUs = self.data[idx]['AUs']  # e.g. [4, 10, 14]
+        ID = self.data[idx]['img_path'][0:4]
+
+        AU_labels = torch.zeros(len(self.AUs))  # 12 classes
+        ID_labels = torch.zeros(len(self.IDs))  # 41 classes
+
+        for au in AUs:
+            AU_labels[self.AU_label2idx[au]] = 1
+
+        ID_labels[self.ID_label2idx[ID]] = 1
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image, (AU_labels, ID_labels)
+
+
+class BP4D_plus_dataset(Dataset):
+    """
+    accept the json file to construct the dataset.
+    Each line of the json file contains the image path and the AU labels.
+    """
+    def __init__(self, root_path, json_file, transform=None):
+        self.data = self._load_data(json_file)
+        print(f"building dataset from: {json_file}")
+        self.root_path = root_path
+        print(f"dataset path: {self.root_path}")
+        self.transform = transform
+        self.AUs = [1, 2, 4, 6, 7, 10, 12, 14, 15, 17, 23, 24]
+        self.IDs = ['F015', 'M005', 'F057', 'M009', 'M053', 'F078', 'F081', 'M047', 'F004', 'M026', 'M056', 'F058',
+                    'F007', 'M031', 'M052', 'F041', 'F014', 'F074', 'M039', 'F044', 'F056', 'F064', 'F048', 'M040',
+                    'F024', 'F076', 'M030', 'F016', 'F035', 'F062', 'M014', 'F033', 'M058', 'M055', 'M006', 'M048',
+                    'F032', 'M022', 'M036', 'M017', 'F053', 'F002', 'F018', 'F067', 'F050', 'F034', 'F027', 'M001',
+                    'F029', 'F012', 'F010', 'M044', 'F079', 'M013', 'M043', 'F059', 'M050', 'M023', 'M008', 'F070',
+                    'F011', 'F030', 'F073', 'F069', 'M041', 'F020', 'M054', 'F040', 'M024', 'M034', 'M037', 'F025',
+                    'F008', 'F060', 'F022', 'M045', 'M007', 'M015', 'M042', 'M016', 'M038', 'M057', 'F051', 'F009',
+                    'F082', 'M049', 'M019', 'F047', 'F054', 'M028', 'F075', 'F042', 'M018', 'F017', 'M003', 'F055',
+                    'F046', 'F077', 'F043', 'F065', 'F023', 'F052', 'M012', 'F005', 'M010', 'F013', 'M032', 'M004',
+                    'F037', 'F045', 'M046', 'F021', 'M027', 'M051', 'F019', 'F003', 'F068', 'M029', 'M025', 'F028',
+                    'M021', 'M020', 'F036', 'F031', 'M002', 'M011', 'F080', 'F066', 'F071', 'F038', 'M035', 'F063',
+                    'F026', 'M033', 'F072', 'F049', 'F061', 'F006', 'F001', 'F039']
+        self.AU_label2idx = {label: idx for idx, label in enumerate(self.AUs)}
+        self.ID_label2idx = {label: idx for idx, label in enumerate(self.IDs)}
+
+    def _load_data(self, json_file):
+        dict_list = []
+        with open(json_file, 'r') as file:
+            for line in file:
+                loaded_dict = json.loads(line)
+                dict_list.append(loaded_dict)
+        return dict_list
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        image_path = self.root_path + self.data[idx]['img_path']
+        image = Image.open(image_path).convert('RGB')
+
+        # Convert label indices to binary representation
+        AUs = self.data[idx]['AUs']  # e.g. [4, 10, 14]
+        ID = self.data[idx]['img_path'][0:4]
+
+        AU_labels = torch.zeros(len(self.AUs))  # 12 classes
+        ID_labels = torch.zeros(len(self.IDs))  # 140 classes
+
+        for au in AUs:
+            AU_labels[self.AU_label2idx[au]] = 1
+
+        ID_labels[self.ID_label2idx[ID]] = 1
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image, (AU_labels, ID_labels)
